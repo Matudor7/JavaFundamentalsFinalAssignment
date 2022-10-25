d3dc0fdb35e39325888b8072bd31699690970b7d
Tudor <90045862+Matudor7@users.noreply.github.com>
2022-10-25 14:45:38 +0200

    Add files via upload

---
+++ 678549-TudorMateiNosca-EndAssignment/src/main/java/librarymanager/Main.java
@@ -0,0 +1,61 @@
+package main.java.librarymanager;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import main.java.librarymanager.data.Database;
+import main.java.librarymanager.model.Item;
+import main.java.librarymanager.model.Member;
+
+import java.io.IOException;
+import java.util.List;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("../../resources/login.fxml"));
+        primaryStage.setTitle("Login");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent windowEvent) {
+                try {
+                    System.out.println("Closing window...");
+                    Database db = new Database();
+
+                    List<Item> items = db.GetAllItems();
+
+                    for(Item item : items){
+                        System.out.println(item.getItemCode() + ". " + item.getTitle());
+                    }
+
+                    db.CloseStreams();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        launch(args);
+
+        Database db = new Database();
+
+        List<Item> items = db.GetAllItems();
+
+        for(Item item : items){
+            System.out.println(item.getItemCode() + ". " + item.getTitle());
+        }
+    }
+}
---
+++ 678549-TudorMateiNosca-EndAssignment/src/main/java/librarymanager/controller/Login.java
@@ -0,0 +1,74 @@
+package main.java.librarymanager.controller;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import main.java.librarymanager.Main;
+import main.java.librarymanager.data.Database;
+import main.java.librarymanager.model.User;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class Login implements Initializable {
+    Database db;
+
+    @FXML
+    TextField usernameTextField;
+
+    @FXML
+    PasswordField passwordField;
+
+    @FXML
+    Button loginButton;
+
+    @FXML
+    Label errorLabel;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        errorLabel.setText("");
+    }
+
+    public void loginButton(ActionEvent event) throws IOException, ClassNotFoundException {
+        db = new Database();
+        User loggingUser = new User(usernameTextField.getText(), passwordField.getText());
+
+        List<User> validUsers = db.GetAllUsers();
+
+        for(User user : validUsers){
+            if(loggingUser.getUsername().equals(user.getUsername())){
+                if(loggingUser.getPassword().equals(user.getPassword())){
+                    LoadMainWindow(event, user);
+                    break;
+                }
+            } else{
+                errorLabel.setText("Invalid username or password");
+            }
+        }
+    }
+
+    public void LoadMainWindow(ActionEvent event, User user) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("../../resources/MainWindow.fxml"));
+
+        MainWindow mainWindowController = new MainWindow();
+        fxmlLoader.setController(mainWindowController);
+        Scene scene = new Scene(fxmlLoader.load());
+
+        Stage stage = new Stage();
+
+        stage.setScene(scene);
+        stage.show();
+        mainWindowController.DisplayWelcomeMessage(user.getName());
+    }
+}
